f0a27afc80808578655e5beb8bc82954aa58273b
Caldwell10 <caldwell.kibe@strathmore.edu>
2023-09-23 23:05:17 +0300

    Added a <tbody> and <thead> tag and added bootstrap to the code

--- viewingfile/viewUsers.php
+++ viewingfile/viewUsers.php
@@ -2,18 +2,29 @@
 include_once "db_connect.php";
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> User list</title>
+
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+  
+
 <div class =header>
     <h1>Header</h1>
 </div>
 
 <div class ="content_section_row">
     
-
-<table>
-<h1>List of Users</h1>
-                <table>
-                    <tr>
-                        <th>#</th>
+<h1>List of users</h1>
+  <table>
+                     <tr>
+                       <th>#</th>
                         <th>Author's name</th>
                         <th>Username</th>
                         <th>Email</th>
@@ -22,7 +33,7 @@ include_once "db_connect.php";
 
       <?php
         $userId=$_GET['userId'];
-        $spot_author="SELECT * FROM `authors` WHERE $userID=`userId` ORDER BY ASC";
+        $spot_author="SELECT * FROM `authors` WHERE $userId=`userId` ORDER BY ASC";
         $res_spot_author=$conn->query($spot_author);
 
         if($res_spot_author->num_rows >0){
@@ -48,5 +59,11 @@ include_once "db_connect.php";
 
 </table>
 
-
 </div>
+ <!-- Add Bootstrap JS and jQuery scripts here (at the end of the body) -->
+ <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
